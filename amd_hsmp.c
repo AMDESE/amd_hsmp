ce1c7a2fc1433c229b79818f34d056129a7536c7
Nathan Fontenot <nathan.fontenot@amd.com>
2021-10-04 14:42:34 -0500

    Revert update to use sysfs_emit.
    This is to ease customer backports of the driver to pre 5.10 kernels.
    
    Signed-off-by: Nathan Fontenot <nathan.fontenot@amd.com>

--- amd_hsmp.c
+++ amd_hsmp.c
@@ -1105,8 +1105,8 @@ static ssize_t smu_firmware_version_show(struct kobject *kobj,
 					 struct kobj_attribute *attr,
 					 char *buf)
 {
-	return sysfs_emit(buf, "%u.%u.%u\n", amd_smu_fw.ver.major,
-			  amd_smu_fw.ver.minor, amd_smu_fw.ver.debug);
+	return sprintf(buf, "%u.%u.%u\n", amd_smu_fw.ver.major,
+		       amd_smu_fw.ver.minor, amd_smu_fw.ver.debug);
 }
 static FILE_ATTR_RO(smu_firmware_version);
 
@@ -1114,7 +1114,7 @@ static ssize_t hsmp_protocol_version_show(struct kobject *kobj,
 					  struct kobj_attribute *attr,
 					  char *buf)
 {
-	return sysfs_emit(buf, "%u\n", amd_hsmp_proto_ver);
+	return sprintf(buf, "%u\n", amd_hsmp_proto_ver);
 }
 static FILE_ATTR_RO(hsmp_protocol_version);
 
@@ -1193,7 +1193,7 @@ static ssize_t boost_limit_show(struct kobject *kobj,
 	if (err)
 		return err;
 
-	return sysfs_emit(buf, "%u\n", limit_mhz);
+	return sprintf(buf, "%u\n", limit_mhz);
 }
 static FILE_ATTR_RW(boost_limit);
 
@@ -1207,7 +1207,7 @@ static ssize_t power_show(struct kobject *kobj,
 	if (err)
 		return err;
 
-	return sysfs_emit(buf, "%u\n", power_mw);
+	return sprintf(buf, "%u\n", power_mw);
 }
 static FILE_ATTR_RO(power);
 
@@ -1241,7 +1241,7 @@ static ssize_t power_limit_show(struct kobject *kobj,
 	if (err)
 		return err;
 
-	return sysfs_emit(buf, "%u\n", limit_mw);
+	return sprintf(buf, "%u\n", limit_mw);
 }
 static FILE_ATTR_RW(power_limit);
 
@@ -1256,7 +1256,7 @@ static ssize_t power_limit_max_show(struct kobject *kobj,
 	if (err)
 		return err;
 
-	return sysfs_emit(buf, "%u\n", limit_mw);
+	return sprintf(buf, "%u\n", limit_mw);
 }
 static FILE_ATTR_RO(power_limit_max);
 
@@ -1271,7 +1271,7 @@ static ssize_t proc_hot_show(struct kobject *kobj,
 	if (err)
 		return err;
 
-	return sysfs_emit(buf, "%s\n", proc_hot ? "active" : "inactive");
+	return sprintf(buf, "%s\n", proc_hot ? "active" : "inactive");
 }
 static FILE_ATTR_RO(proc_hot);
 
@@ -1302,7 +1302,7 @@ static ssize_t xgmi_pstate_show(struct kobject *kobj,
 	if (err)
 		return err;
 
-	return sysfs_emit(buf, "%d\n", pstate);
+	return sprintf(buf, "%d\n", pstate);
 }
 static FILE_ATTR_RW(xgmi_pstate);
 
@@ -1316,7 +1316,7 @@ static ssize_t xgmi_speed_show(struct kobject *kobj,
 	if (err)
 		return err;
 
-	return sysfs_emit(buf, "%d\n", speed);
+	return sprintf(buf, "%d\n", speed);
 }
 static FILE_ATTR_RO(xgmi_speed);
 
@@ -1350,7 +1350,7 @@ static ssize_t fabric_clocks_show(struct kobject *kobj,
 	if (err)
 		return err;
 
-	return sysfs_emit(buf, "%u,%u\n", fclk, memclk);
+	return sprintf(buf, "%u,%u\n", fclk, memclk);
 }
 static FILE_ATTR_RO(fabric_clocks);
 
@@ -1365,7 +1365,7 @@ static ssize_t cclk_limit_show(struct kobject *kobj,
 	if (err)
 		return err;
 
-	return sysfs_emit(buf, "%u\n", max_mhz);
+	return sprintf(buf, "%u\n", max_mhz);
 }
 static FILE_ATTR_RO(cclk_limit);
 
@@ -1380,7 +1380,7 @@ static ssize_t c0_residency_show(struct kobject *kobj,
 	if (err)
 		return err;
 
-	return sysfs_emit(buf, "%u\n", residency);
+	return sprintf(buf, "%u\n", residency);
 }
 static FILE_ATTR_RO(c0_residency);
 
@@ -1417,7 +1417,7 @@ static ssize_t tctl_show(struct kobject *kobj,
 	if (err)
 		return err;
 
-	return sysfs_emit(buf, "%u\n", tctl);
+	return sprintf(buf, "%u\n", tctl);
 }
 static FILE_ATTR_RO(tctl);
 
@@ -1432,7 +1432,7 @@ static ssize_t ddr_max_bandwidth_show(struct kobject *kobj,
 	if (err)
 		return err;
 
-	return sysfs_emit(buf, "%u\n", ddr_bw.max_bandwidth);
+	return sprintf(buf, "%u\n", ddr_bw.max_bandwidth);
 }
 static FILE_ATTR_RO(ddr_max_bandwidth);
 
@@ -1447,7 +1447,7 @@ static ssize_t ddr_utilized_bandwidth_show(struct kobject *kobj,
 	if (err)
 		return err;
 
-	return sysfs_emit(buf, "%u\n", ddr_bw.utilized_bandwidth);
+	return sprintf(buf, "%u\n", ddr_bw.utilized_bandwidth);
 }
 static FILE_ATTR_RO(ddr_utilized_bandwidth);
 
@@ -1462,7 +1462,7 @@ static ssize_t ddr_percent_utilized_show(struct kobject *kobj,
 	if (err)
 		return err;
 
-	return sysfs_emit(buf, "%u\n", ddr_bw.utilized_percent);
+	return sprintf(buf, "%u\n", ddr_bw.utilized_percent);
 }
 static FILE_ATTR_RO(ddr_percent_utilized);
 
